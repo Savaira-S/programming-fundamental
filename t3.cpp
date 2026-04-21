2755c43345197824c4c2dcd7fe7a4f3f84dce435
Savaira-S <savairaeman@gmail.com>
2026-04-21 20:04:09 +0500

    Add files via upload

---
+++ t3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+int num1,num2;
+cout<<"enter number 1: ";
+cin>>num1;
+cout<<"enter number 2: ";
+cin>>num2;
+cout<<"greater number: "<<max(num1,num2);
+
+return 0;
+}
